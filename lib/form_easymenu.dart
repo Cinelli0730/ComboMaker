5b97a416936dcf954adbc8602a388fda415f4a97
Cinelli0730 <cyclesymphony70@gmail.com>
2023-12-11 01:39:20 +0900

    Create Output Display Combo

--- lib/form_easymenu.dart
+++ lib/form_easymenu.dart
@@ -1,59 +1,130 @@
+//flutter package
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:combo_maker/common/constants.dart';
-import 'json/framedata.dart';
 
-class HomeScreen extends ConsumerWidget {
-  // 今回解説しておりませんがriverpodを使用しております。
-  // あくまで緑色のステータスバー的なものを動かすためだけですので、
-  // なくてもスクロールページは実装できます。
-  final alignmentProvider = StateProvider<double>((ref) => -1.0);
+//firebase package
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'firebase_options.dart';
 
-  HomeScreen({super.key});
+//My package
+import 'cloud_manager/readfile.dart';
+import 'common/constants.dart';
+//import 'common/moves.dart';
+import 'firestore/firestore.dart';
+import 'package:combo_maker/json/framedata.dart';
+
+final pageNumProvider = StateProvider<double>((ref) => -1.0);
+final displayComboProvider = StateProvider<String>((ref) => "");
+
+class ComboMaker2 extends ConsumerStatefulWidget {
+  const ComboMaker2({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    String result = 'test\\nl2\\nl3\\nl4\\n';
+  ConsumerState<ComboMaker2> createState() => _SetDataState();
+}
+
+class _SetDataState extends ConsumerState<ComboMaker2> {
+  String appDefaultPath = "";
+  String display1 = 'path = ';
+  String display2 = 'Firebase Document';
+  List<DocumentSnapshot> documentList = [];
+
+  //double? _deviceWidth, _deviceHeight;
+
+  //ファイル読み込み用
+  String downloadFolderPath = '';
+  String downloadFileFullPath = '';
+  String fileName = fileFrameData;
+  String sheetName = 'A.K.I.';
+  List<List<dynamic>> readExcel =
+      List.empty(); // = await importExcel(importPath, fileName, sheetName);
+  File? file;
+  Directory? appDocDir;
+  Uint8List? byteDlFile;
+  Uint8List? byteLocalFile;
+  String displayCombo = "";
+  List<FrameData> mFrameDataList = List.empty();
+
+  //firestore用
+  MyFirestore myFirestore = MyFirestore();
+
+  _SetDataState();
+  void buttonPressed(String text) {
+    setState(() {
+      displayCombo += text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    CarouselController buttonCarouselController = CarouselController();
     //final _deviceWidth = MediaQuery.of(context).size.width; //画面の横幅
-    //final _deviceHeight = MediaQuery.of(context).size.height; //画面の縦幅
-    FrameData mFrameData = FrameData();
-    mFrameData.command1 = "LP";
-    mFrameData.moveName = "立ち弱P （蛇突）";
-    mFrameData.startUp = 5;
-    mFrameData.active = 2;
-    mFrameData.recovery = 7;
-    mFrameData.hitStun = 4;
-    mFrameData.blockStun = -1;
-    mFrameData.cancelType = "C";
-    mFrameData.damage = 300;
-    mFrameData.scaling = 10;
-    mFrameData.dGageUp = 250;
-    mFrameData.dGageDown = -500;
-    mFrameData.dGageCounter = -2000;
-    mFrameData.sGageUp = 300;
-    mFrameData.properties = "上";
-    FrameData mFrameData2 = FrameData();
-    mFrameData2.command1 = "LK";
-    mFrameData2.moveName = "立ち弱K （蛇咬脚）";
-    mFrameData2.startUp = 5;
-    mFrameData2.active = 3;
-    mFrameData2.recovery = 11;
-    mFrameData2.hitStun = 4;
-    mFrameData2.blockStun = -2;
-    mFrameData2.cancelType = "C";
-    mFrameData2.damage = 300;
-    mFrameData2.scaling = 10;
-    mFrameData2.dGageUp = 250;
-    mFrameData2.dGageDown = -500;
-    mFrameData2.dGageCounter = -2000;
-    mFrameData2.sGageUp = 300;
-    mFrameData2.properties = "上";
-    List<FrameData> testFrameDataList = List.empty();
-    testFrameDataList = testFrameDataList.toList();
-    testFrameDataList.add(mFrameData);
-    testFrameDataList.add(mFrameData2);
+    final _deviceHeight = MediaQuery.of(context).size.height; //画面の縦幅
+    final dataList = [
+      "ジャンプ弱P （空蛇突）",
+      "ジャンプ弱K （空蛇蹴）",
+      "ジャンプ中P （回旋蛇）",
+      "ジャンプ中K （蛇天蹴）",
+      "ジャンプ強P （騰空双蛇突）",
+      "ジャンプ強K （騰空双蹴蛇）",
+      "蒲牢",
+      "?吻",
+      "囚牛",
+      "蚣蝮",
+      "渾沌 （2段目）",
+      "窮奇 （2段目）",
+      "紫煙砲",
+      "紫煙追",
+      "OD 紫煙砲",
+      "OD 紫煙追",
+      "紫煙追 （炸裂）",
+      "OD 紫煙追 （炸裂）",
+      "紫泡泉",
+      "紫泡撒",
+      "弱 蛇頭鞭",
+      "中 蛇頭鞭",
+      "強 蛇頭鞭",
+      "OD 蛇頭鞭",
+      "弱 凶襲突",
+      "中 凶襲突",
+      "強 凶襲突",
+      "OD 凶襲突",
+      "弱 蛇軽功",
+      "中 蛇軽功",
+      "強 蛇軽功",
+      "OD 蛇軽功",
+      "悪鬼蛇行",
+      "猛毒牙",
+      "蛇連咬",
+      "雁字搦",
+      "SA1 死屍累々",
+      "SA2 紫煙裂爪",
+      "SA3 睚眦",
+      "CA 睚眦",
+      "贔屓",
+      "饕餮",
+      "前方ステップ",
+      "後方ステップ",
+      "ドライブインパクト （鑿歯）",
+      "ドライブリバーサル （封豕）",
+      "ドライブパリィ",
+      "ジャストパリィ（打撃）",
+      "ジャストパリィ（飛び道具）",
+      "パリィドライブラッシュ",
+      "キャンセルドライブラッシュ",
+    ];
+    List<FrameData> testFrameDataList = List.empty(growable: true);
+    for (int i = 0; i < dataList.length; i++) {
+      FrameData mFrameData = FrameData();
+      mFrameData.moveName = dataList[i];
+      testFrameDataList.add(mFrameData);
+    }
     return Directionality(
         textDirection: TextDirection.ltr,
         //body: SafeArea(
@@ -61,32 +132,35 @@ class HomeScreen extends ConsumerWidget {
             body: SafeArea(
           child: Column(
             children: [
-              Expanded(
-                flex: 60,
+              SizedBox(
+                height: _deviceHeight * 0.55,
+                width: double.infinity,
                 child: Container(
-                  width: double.infinity,
                   color: Colors.black,
                   constraints: const BoxConstraints.tightForFinite(
                       width: double.infinity),
                   child: Wrap(
                     crossAxisAlignment: WrapCrossAlignment.start,
                     children: [
-                      Text(
-                        result,
-                        style: const TextStyle(
-                          color: Colors.blue,
-                          fontSize: 40,
-                        ),
+                      Consumer(
+                        builder: (context, ref, child) {
+                          return Text(
+                            ref.watch(displayComboProvider),
+                            style: const TextStyle(
+                              color: Colors.blue,
+                              fontSize: 30,
+                            ),
+                          );
+                        },
                       ),
                     ],
                   ),
                 ),
               ),
-              Expanded(
-                flex: 5,
-                child: SizedBox(
-                  height: 5,
-                  child: Row(
+              SizedBox(
+                height: _deviceHeight * 0.05,
+                child: Consumer(builder: (context, ref, child) {
+                  return Row(
                     children: [
                       Expanded(
                         child: Container(
@@ -94,7 +168,11 @@ class HomeScreen extends ConsumerWidget {
                           constraints: const BoxConstraints.tightForFinite(
                               width: double.infinity),
                           alignment: Alignment.center,
-                          child: const Text("通常技"),
+                          child: TextButton(
+                            onPressed: () =>
+                                buttonCarouselController.jumpToPage(0),
+                            child: const Text("通常技"),
+                          ),
                         ),
                       ),
                       Expanded(
@@ -103,7 +181,11 @@ class HomeScreen extends ConsumerWidget {
                           constraints: const BoxConstraints.tightForFinite(
                               width: double.infinity),
                           alignment: Alignment.center,
-                          child: const Text("特殊技"),
+                          child: TextButton(
+                            onPressed: () =>
+                                buttonCarouselController.jumpToPage(1),
+                            child: const Text("特殊技"),
+                          ),
                         ),
                       ),
                       Expanded(
@@ -112,7 +194,11 @@ class HomeScreen extends ConsumerWidget {
                           constraints: const BoxConstraints.tightForFinite(
                               width: double.infinity),
                           alignment: Alignment.center,
-                          child: const Text("必殺技"),
+                          child: TextButton(
+                            onPressed: () =>
+                                buttonCarouselController.jumpToPage(2),
+                            child: const Text("必殺技"),
+                          ),
                         ),
                       ),
                       Expanded(
@@ -121,17 +207,21 @@ class HomeScreen extends ConsumerWidget {
                           constraints: const BoxConstraints.tightForFinite(
                               width: double.infinity),
                           alignment: Alignment.center,
-                          child: const Text("通常行動"),
+                          child: TextButton(
+                            onPressed: () =>
+                                buttonCarouselController.jumpToPage(3),
+                            child: const Text("通常行動"),
+                          ),
                         ),
                       ),
                     ],
-                  ),
-                ),
+                  );
+                }),
               ),
               Consumer(
                 builder: (context, ref, child) {
                   return AnimatedAlign(
-                    alignment: Alignment(ref.watch(alignmentProvider), 0),
+                    alignment: Alignment(ref.watch(pageNumProvider), 0),
                     duration: const Duration(milliseconds: 1),
                     child: Container(
                       color: Colors.green,
@@ -143,37 +233,46 @@ class HomeScreen extends ConsumerWidget {
               ),
               Consumer(
                 builder: (context, ref, child) {
-                  return CarouselSlider(
-                    options: CarouselOptions(
-                      //enableInfiniteScroll: false,
-                      height: 200,
-                      viewportFraction: 1,
-                      onScrolled: (value) {
-                        double revalue = value! % 4;
-                        double result = (revalue * 2 / 3 - 1);
-                        //stateは-1～+1,resultは0～item要素数の間で変化する
-                        ref
-                            .read(alignmentProvider.notifier)
-                            .update((state) => result);
-                      },
-                    ),
-                    // このitemsの中に表示したいウィジェットを入れてください。
-                    items: [
-                      Page(
-                        name: 'Page1',
-                        frameDataList: testFrameDataList,
-                      ),
-                      Page(
-                        name: 'Page2',
-                        frameDataList: testFrameDataList,
-                      ),
-                      Page(
-                        name: 'Page3',
-                        frameDataList: testFrameDataList,
-                      ),
-                      Page(
-                        name: 'Page4',
-                        frameDataList: testFrameDataList,
+                  return Column(
+                    children: [
+                      CarouselSlider(
+                        carouselController: buttonCarouselController,
+                        options: CarouselOptions(
+                          //enableInfiniteScroll: false,
+                          height: _deviceHeight * 0.3,
+                          viewportFraction: 1,
+                          onScrolled: (value) {
+                            double revalue = value! % 4;
+                            double result = (revalue * 2 / 3 - 1);
+                            //stateは-1～+1,resultは0～item要素数の間で変化する
+                            ref
+                                .read(pageNumProvider.notifier)
+                                .update((state) => result);
+                          },
+                        ),
+                        // このitemsの中に表示したいウィジェットを入れてください。
+                        items: [
+                          Page(
+                            name: 'Page1',
+                            frameDataList: testFrameDataList,
+                            btnCarouselController: buttonCarouselController,
+                          ),
+                          Page(
+                            name: 'Page2',
+                            frameDataList: testFrameDataList,
+                            btnCarouselController: buttonCarouselController,
+                          ),
+                          Page(
+                            name: 'Page3',
+                            frameDataList: testFrameDataList,
+                            btnCarouselController: buttonCarouselController,
+                          ),
+                          Page(
+                            name: 'Page4',
+                            frameDataList: testFrameDataList,
+                            btnCarouselController: buttonCarouselController,
+                          ),
+                        ],
                       ),
                     ],
                   );
@@ -184,45 +283,128 @@ class HomeScreen extends ConsumerWidget {
         )));
     //);
   }
+
+  void getDocumentList() async {
+    List<String> dataList = List.empty();
+    final collectionRef = FirebaseFirestore.instance
+        .collection("StreetFighter6")
+        .doc("Contents")
+        .collection("Characters"); // CollectionReference
+    final querySnapshot = await collectionRef.get(); // QuerySnapshot
+    final queryDocSnapshot = querySnapshot.docs; // List<QueryDocumentSnapshot>
+    for (final snapshot in queryDocSnapshot) {
+      dataList = dataList.toList();
+      dataList.add(snapshot.data().toString()); // `data()`で中身を取り出す
+    }
+  }
+
+  void getPath() async {
+    appDefaultPath = await _localPath;
+    setState(() {
+      display1 = display1 + appDefaultPath;
+    });
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
 }
 
 class Page extends StatelessWidget {
   final String name;
   final List<FrameData> frameDataList;
-  const Page({super.key, required this.name, required this.frameDataList});
+  final CarouselController btnCarouselController;
+  const Page(
+      {super.key,
+      required this.name,
+      required this.frameDataList,
+      required this.btnCarouselController});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: kWhite,
-      child: Row(children: [
-        BtnParts(name: frameDataList[0].moveName),
-        BtnParts(name: frameDataList[0].moveName),
-        /*Text(
-          name,
-          style: const TextStyle(fontSize: 24.0, color: kRed),
-        ),*/
-      ]),
+    List<Widget> widgetList = List.empty();
+    //画面サイズを取得
+    final _deviceWidth = MediaQuery.of(context).size.width; //画面の横幅
+    final _deviceHeight = MediaQuery.of(context).size.height; //画面の縦幅
+    for (int i = 0; i < frameDataList.length; i++) {
+      widgetList = widgetList.toList();
+      widgetList.add(BtnParts(name: frameDataList[i].moveName));
+    }
+    return Row(
+      children: [
+        SizedBox(
+          height: double.infinity,
+          width: _deviceWidth * 0.05,
+          child: TextButton(
+            onPressed: () => btnCarouselController.previousPage(
+                duration: const Duration(milliseconds: 1),
+                curve: Curves.linear),
+            child: const Text("<"),
+          ),
+        ),
+        SizedBox(
+          width: _deviceWidth * 0.9, //* 70 / 100,
+          height: _deviceHeight * 0.5,
+          //color: kWhite,
+          child: Wrap(
+              crossAxisAlignment: WrapCrossAlignment.start,
+              children: widgetList),
+        ),
+        SizedBox(
+          height: double.infinity,
+          width: _deviceWidth * 0.05,
+          child: TextButton(
+            onPressed: () => btnCarouselController.nextPage(
+                duration: const Duration(milliseconds: 1),
+                curve: Curves.linear),
+            child: const Text(">"),
+          ),
+        ),
+      ],
     );
   }
 }
 
-class BtnParts extends StatelessWidget {
+class BtnParts extends ConsumerWidget {
   final String name;
+  static String comboDisplay = "";
   const BtnParts({super.key, required this.name});
 
   @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () async {
-        if (kDebugMode) {
-          print("btn is pushed");
-        }
+  Widget build(BuildContext context, WidgetRef ref) {
+    //画面サイズを取得
+    final _deviceWidth = MediaQuery.of(context).size.width; //画面の横幅
+    final _deviceHeight = MediaQuery.of(context).size.height; //画面の縦幅
+    final _btnRowNum = 13;
+    return Consumer(
+      builder: (context, ref, child) {
+        return SizedBox(
+          width: _deviceWidth / 4 * 0.9,
+          height: _deviceHeight / (4 * _btnRowNum + 1),
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: TextButton(
+              onPressed: () {
+                comboDisplay = ref.read(displayComboProvider);
+                if (comboDisplay == "") {
+                  ref
+                      .read(displayComboProvider.notifier)
+                      .update((state) => name);
+                } else {
+                  ref
+                      .read(displayComboProvider.notifier)
+                      .update((state) => "$state->$name");
+                }
+              },
+              child: Text(name),
+            ),
+          ),
+        );
       },
-      child: Text(name),
     );
   }
 }
 
 void main(List<String> args) {
-  runApp(ProviderScope(child: HomeScreen()));
+  runApp(const ProviderScope(child: ComboMaker2()));
 }
